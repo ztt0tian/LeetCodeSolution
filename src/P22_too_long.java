64af0ea83dba6bad5546763beacc80e3dfe1ab06
zhaotian <1290507445@qq.com>
2018-07-05 14:19:35 +0800

    22th problem need too long time

---
+++ src/P22_too_long.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * @author zhaotian
+ * @date 2018/7/3 16:15
+ */
+//给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+//
+//        例如，给出 n = 3，生成结果为：
+//
+//        [
+//        "((()))",
+//        "(()())",
+//        "(())()",
+//        "()(())",
+//        "()()()"
+//        ]
+//       勉强通过，但是时间花费太高了
+public class P22_too_long {
+    public static void main(String[] args) {
+        List<String> res=generateParenthesis(4);
+        for(String s:res){
+            System.out.println(s);
+        }
+    }
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if (n <= 0) {
+            return null;
+        }
+        if(n==1) {
+            res.add("()");
+            return res;
+        }
+        else {
+            for(String each_str:generateParenthesis(n-1)) {
+                res.add(each_str+"(" + ")");
+                int start=0;
+                int index;
+                while ((index=each_str.indexOf("(",start))!=-1){
+                    res.add(each_str.substring(0,index+1)+"()"+each_str.substring(index+1));
+                    start=index+1;
+                }
+                res.remove(each_str);
+                //去重
+                Set<String> stringSet=new HashSet<>();
+                stringSet.addAll(res);
+                res.clear();
+                res.addAll(stringSet);
+
+            }
+
+        }
+        return res;
+    }
+}
